f99b59f02218247e9a329fde05ddf33d5fa7cf0c
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-05-18 11:14:09 -0300

    Criado novo projeto
    Criado novo projeto com o objetivo de fazer um app que gere uma imagem através de um prompt utilizando uma IA.

---
+++ ImageGeneratorIA/Services/API.swift
@@ -0,0 +1,8 @@
+//
+//  API.swift
+//  ImageGeneratorIA
+//
+//  Created by Pedro Henrique on 18/05/23.
+//
+
+import Foundation
---
+++ ImageGeneratorIA/ViewModel/GeneratorImageManager.swift
@@ -0,0 +1,8 @@
+//
+//  GeneratorImageManager.swift
+//  ImageGeneratorIA
+//
+//  Created by Pedro Henrique on 18/05/23.
+//
+
+import Foundation
