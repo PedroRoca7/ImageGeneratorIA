0cbf78c6ea0fbd47a14167eb8a52823937992c18
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-05-18 10:49:24 -0300

    Initial Commit

---
+++ ImageGeneratorIA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGeneratorIA
+//
+//  Created by Pedro Henrique on 18/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
